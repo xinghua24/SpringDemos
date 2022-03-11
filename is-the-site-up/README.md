07a359ab02b7c79e115991197fe81696b74d286b
Xing <xinghua24@gmail.com>
2022-03-10 21:06:17 -0500

    add is-the-site-up project created by vs code

---
+++ is-the-site-up/README.md
@@ -0,0 +1,2 @@
+Test Creating Spring Project using VS Code
+https://www.youtube.com/watch?v=uq4GjRF_860
---
+++ is-the-site-up/src/test/java/com/example/isthesiteup/IsTheSiteUpApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.isthesiteup;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IsTheSiteUpApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
