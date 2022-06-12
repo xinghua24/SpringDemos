89167c2da7701c5ad5498e5a8e4d503e1f750371
Xinghua Zhang <xinghua24@gmail.com>
2022-06-12 01:12:15 -0400

    update application.properties file

--- spring-data-mongo-demo/README.md
+++
@@ -1,3 +0,0 @@
-https://www.baeldung.com/spring-data-mongodb-tutorial
-
-Repository: https://github.com/eugenp/tutorials/blob/master/persistence-modules/spring-data-mongodb/src/main/java/com/baeldung/model/User.java
--- spring-data-mongo-demo/src/main/java/com/example/repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.example.repository;
-
-import com.example.model.User;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-public interface UserRepository extends MongoRepository<User, String> {
-}
